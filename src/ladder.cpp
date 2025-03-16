135953265d5c2c5e3c2bdfc423623d3e7da3c82a
YuhaoW-Ubuntu <yuhaow29@uci.edu>
2025-03-16 16:36:13 -0700

    commit

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -5,40 +5,59 @@ void error(string word1, string word2, string msg) {
 }
 
 bool edit_distance_within(const std::string& str1, const std::string& str2, int d) {
+    int n = str1.size(), m = str2.size();
+    if (abs(n - m) > d) return false;
 
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int i = 0; i <= n; i++) {
+        dp[i][0] = i;
+    }
+    for (int j = 0; j <= m; j++) {
+        dp[0][j] = j;
+    }
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= m; j++){
+            if (str1[i - 1] == str2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1];
+            else
+                dp[i][j] = 1 + min(min(dp[i - 1][j],dp[i][j - 1]),dp[i - 1][j - 1]);
+        }
+    }
+    return dp[n][m] <= d;
 }
 
 bool is_adjacent(const string& word1, const string& word2) {
-    int l1 = word1.length(), l2 = word2.length();
-    if (abs(l1 - l2) > 1) return false;
-    int count = 0;
-
-    if (l1 == l2) {
-        for (int i = 0; i < l1; ++i) {
-            if (word1[i] != word2[i]) {
-                ++count;
-                if (count >= 2)return false;
-            }
-        }
-        return true;
-    }
-
-    if (l1 < l2) return is_adjacent(word2, word1);
-
-
-    int i = 0, j = 0;
-    bool flag = false;
-    while (i < l1 && j < l2) {
-        if (word1[i] == word2[j]) {
-            ++i;
-            ++j;
-        }else {
-            if (flag) return false;
-            flag = true;
-            i++;
-        }
-    }
-    return true;
+    // int l1 = word1.length(), l2 = word2.length();
+    // if (abs(l1 - l2) > 1) return false;
+    // int count = 0;
+    //
+    // if (l1 == l2) {
+    //     for (int i = 0; i < l1; ++i) {
+    //         if (word1[i] != word2[i]) {
+    //             ++count;
+    //             if (count >= 2)return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    //
+    // if (l1 < l2) return is_adjacent(word2, word1);
+    //
+    // int i = 0, j = 0;
+    // bool flag = false;
+    // while (i < l1 && j < l2) {
+    //     if (word1[i] == word2[j]) {
+    //         ++i;
+    //         ++j;
+    //     }else {
+    //         if (flag) return false;
+    //         flag = true;
+    //         i++;
+    //     }
+    // }
+    // return true;
+    return edit_distance_within(word1, word2, 1);
 }
 
 vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -5,7 +5,7 @@ int main() {
     set<string> word_list;
     load_words(word_list, file_name);
 
-    vector<string> ladder = generate_word_ladder("abase", "abated", word_list);
+    vector<string> ladder = generate_word_ladder("cold", "warm", word_list);
 
     print_word_ladder(ladder);
 }
